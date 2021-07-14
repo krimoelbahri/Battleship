4e2e59b0c90e0dd65a5e7caba68d9d043c1fc113
karim <k.guezlane@gmail.com>
2021-07-14 12:13:11 +0100

    ships factory function

---
+++ src/ships.js
@@ -0,0 +1,18 @@
+const Ship=function(length){
+	let position=[];
+	let hitArray=[];
+	const hit= function(coordinates){
+		for(let i=0;i< this.position.length;i++){
+			if(this.position[i].includes(coordinates[0]) && this.position[i].includes(coordinates[1])){
+				hitArray.push(coordinates);
+				return true;
+			} 
+		}
+	};
+	const isSunk= function(){
+		if(hitArray.length===length){return true;}
+		return false; 
+	};
+	return({hitArray,position,hit,isSunk});
+};
+export {Ship};
