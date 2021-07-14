ac15202451b9c0c522f2b8ac217c4b3f2df3d1f6
karim <k.guezlane@gmail.com>
2021-07-14 16:00:40 +0100

    added receive attack methode

--- src/game_board.js
+++ src/game_board.js
@@ -12,8 +12,11 @@ const GameBoard= function(){
 		["","","","","","","","","",""],
 	];
 
-	const receiveAttack = function(){};
-	const arrangeShips = function(){};
+	const receiveAttack = function(row,column){
+		if(gameBoard[row][column]===""){return gameBoard[row][column]="missed";}
+		if(gameBoard[row][column].hit([row,column])){return gameBoard[row][column]="hit";}
+	};
+	
 	return{gameBoard,receiveAttack,arrangeShips};
 };
 export {GameBoard};
