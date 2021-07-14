c5b432c38939334896f99a1dd8d7169a81ab9c6d
karim <k.guezlane@gmail.com>
2021-07-14 16:02:29 +0100

    checking game board for existig place

--- src/game_board.js
+++ src/game_board.js
@@ -16,6 +16,20 @@ const GameBoard= function(){
 		if(gameBoard[row][column]===""){return gameBoard[row][column]="missed";}
 		if(gameBoard[row][column].hit([row,column])){return gameBoard[row][column]="hit";}
 	};
+	const checkGB= function(ship,position,start){
+		if(position==="horizontal"&& ship.length+start.column<10){
+			for(let i=0;i<ship.length;i++){ 
+				if(gameBoard[start.row][start.column+i]!==""){return false;}
+			}
+		}
+		if(position==="vertical"&& ship.length+start.row<10){
+			for(let i=0;i<ship.length;i++){ 
+				if(gameBoard[start.row+i][start.column]!==""){return false;}
+			}
+		}
+		return true;
+	};
+	
 	
 	return{gameBoard,receiveAttack,arrangeShips};
 };
