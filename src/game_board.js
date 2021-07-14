60732072f0e319e2a47459b7cd60b1e57a78f321
karim <k.guezlane@gmail.com>
2021-07-14 16:03:19 +0100

    added arrange ships methode

--- src/game_board.js
+++ src/game_board.js
@@ -29,8 +29,22 @@ const GameBoard= function(){
 		}
 		return true;
 	};
-	
-	
+	const arrangeShips = function(ship,position,start){
+		if(!checkGB(ship,position,start)){return false;}
+		if(position==="horizontal"&& ship.length+start.column<10){
+			for(let i=0;i<ship.length;i++){ 
+				ship.position.push([start.row,start.column+i]);
+				this.gameBoard[start.row][start.column+i]=ship;
+			}
+		}
+		if(position==="vertical"&& ship.length+start.row<10){
+			for(let i=0;i<ship.length;i++){ 
+				ship.position.push([start.row+i,start.column]);
+				this.gameBoard[start.row+i][start.column]=ship;
+			}
+		}
+		return true;
+	};
 	return{gameBoard,receiveAttack,arrangeShips};
 };
 export {GameBoard};
