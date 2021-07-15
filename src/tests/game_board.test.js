08971cecd29b8eae7462e6987beb6c52db675bf3
karim <k.guezlane@gmail.com>
2021-07-15 16:04:22 +0100

    testing if all ships are sunk

--- src/tests/game_board.test.js
+++ src/tests/game_board.test.js
@@ -4,7 +4,10 @@ import { GameBoard } from "../game_board";
 
 let battleShip1= Ship(5);
 let cruiser= Ship(3);
+let cruiser2= Ship(3);
+let carrier= Ship(2);
 let playerGB= GameBoard();
+
 playerGB.arrangeShips(battleShip1,"vertical",{row:4,column:2});
 test("cruiser Not arranged 1  ", () => {
 	expect(playerGB.arrangeShips(cruiser,"vertical",{row:4,column:2})).toBe(false);
@@ -21,6 +24,9 @@ test("cruiser Not arranged 4 ", () => {
 test("cruiser arranged  ", () => {
 	expect(playerGB.arrangeShips(cruiser,"horizontal",{row:5,column:3})).toBe(true);
 });
+test("carrier arranged  ", () => {
+	expect(playerGB.arrangeShips(carrier,"horizontal",{row:8,column:8})).toBe(true);
+});
 test("battleShip1 hit 1 ", () => {
 	expect(playerGB.receiveAttack(5,2)).toBe("hit");
 });
@@ -33,13 +39,25 @@ test("battleShip1 hit 3 ", () => {
 test("battleShip1 hit 4 ", () => {
 	expect(playerGB.receiveAttack(7,2)).toBe("hit");
 });
+test("battleShip1 hit 5 ", () => {
+	expect(playerGB.receiveAttack(8,2)).toBe("hit");
+});
 test("cruiser hit 1 ", () => {
 	expect(playerGB.receiveAttack(5,3)).toBe("hit");
 });
 test("cruiser hit 2 ", () => {
 	expect(playerGB.receiveAttack(5,4)).toBe("hit");
 });
+test("cruiser hit 3 ", () => {
+	expect(playerGB.receiveAttack(5,5)).toBe("hit");
+});
 test("battleShip1 Not hit 1 ", () => {
 	expect(playerGB.receiveAttack(9,2)).toBe("missed");
+});
+test("all ships are sunk ", () => {
+	expect(playerGB.isAllShipsSunk([battleShip1,cruiser])).toBe(true);
+});
+test("Not all ships are sunk ", () => {
+	expect(playerGB.isAllShipsSunk([battleShip1,cruiser,cruiser2,carrier])).toBe(false);
 });
 
