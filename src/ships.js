7527d2dab9badef7a8da7b08a00e8d235607db41
karim <k.guezlane@gmail.com>
2021-07-22 17:39:18 +0100

    cleaning code

--- src/ships.js
+++ src/ships.js
@@ -1,18 +1,21 @@
 const Ship=function(length){
-	let position=[];
+	const position=[];
 	const hitArray=[];
 	const hit= function(coordinates){
 		for(let i=0;i< this.position.length;i++){
-			if(this.position[i].includes(coordinates[0]) && this.position[i].includes(coordinates[1])){
+			if(this.position[i].includes(coordinates[0]) 
+			&& this.position[i].includes(coordinates[1])){
 				hitArray.push(coordinates);
 				return true;
 			} 
 		}
 	};
 	const isSunk= function(){
-		if(hitArray.length===length){return true;}
+		if(hitArray.length===length){
+			return true;
+		}
 		return false; 
 	};
-	return{length,hitArray,position,hit,isSunk};
+	return{length,position,hit,isSunk};
 };
 export {Ship};
