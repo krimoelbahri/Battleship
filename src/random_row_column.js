4db0754d32c20d8a24a9377bfd84a0f7d7cd364a
karim <k.guezlane@gmail.com>
2021-07-22 20:35:51 +0100

    deleted

--- src/random_row_column.js
+++
@@ -1,6 +0,0 @@
-const getRandomRowColumn= function(){
-	let row=Math.floor(Math.random()*10);
-	let column=Math.floor(Math.random()*10);
-	return{row:row,column:column};
-};
-export {getRandomRowColumn};
