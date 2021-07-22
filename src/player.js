d23e920e5e1f7e9232ad677d81d5f2546dee4144
karim <k.guezlane@gmail.com>
2021-07-22 18:03:05 +0100

    adding ships

--- src/player.js
+++ src/player.js
@@ -2,3 +2,11 @@ import {Ship} from "./ships";
 import {GameBoard} from "./game_board";
 import {getRandomRowColumn} from "./GBoard_handling";
 
+const Player= function(){
+	//ships
+
+	let airCraft=Ship(5);let battleShip=Ship(4);let cruiser=Ship(3);
+	let destroyer1=Ship(2);let destroyer2=Ship(2);let submarine1=Ship(1);let submarine2=Ship(1);
+	
+};
+export {Player};
