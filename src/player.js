3e6034782e84cdc2009ba124ff34524e2791b222
karim <k.guezlane@gmail.com>
2021-07-22 18:14:02 +0100

    added a random arrange method

--- src/player.js
+++ src/player.js
@@ -1,6 +1,8 @@
 import {Ship} from "./ships";
 import {GameBoard} from "./game_board";
 import {getRandomRowColumn} from "./GBoard_handling";
+import {getRandomOrientation} from "./GBoard_handling";
+
 
 const Player= function(){
 	//ships
@@ -12,6 +14,20 @@ const Player= function(){
 	const ships=[airCraft,battleShip,cruiser,destroyer1,destroyer2,submarine1,submarine2];
 	const gameBoard= GameBoard();
 	
+	const randomlyArrange= function(){
+		let i=0;
+		while(i<ships.length){
+			let rndRow=getRandomRowColumn().row;
+			let rndColumn= getRandomRowColumn().column;
+			let orientation= getRandomOrientation();
+			if(gameBoard.arrangeShips(ships[i],orientation,{row:rndRow,column:rndColumn})){
+				i++;
+			}
+		}
+	};
 	
+	
+	
+
 };
 export {Player};
